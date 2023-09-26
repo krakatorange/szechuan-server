8e35a2a66a656481cbb987178c0ca21cd1d74c1d
Muhammad Ibrahim Kamal <139374245+mibrahim0499@users.noreply.github.com>
2023-09-27 00:35:19 +0500

    error handled all

--- routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controller/userController'); // Adjust the path accordingly
-
-// Define routes for user-related actions
-router.post('/addUser', userController.addUser);
-
-module.exports = router;
