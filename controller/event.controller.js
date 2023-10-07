321ab170d11ff465efa4c48573755196f5da7e69
Ibrahim <m.ibrahim0499@gmail.com>
2023-10-07 15:38:53 +0500

    delete button

--- controller/event.controller.js
+++ controller/event.controller.js
@@ -1,5 +1,5 @@
-const multer = require('multer');
-const Event = require('../model/event.model');
+const multer = require("multer");
+const Event = require("../model/event.model");
 
 // Configure Multer storage
 const storage = multer.memoryStorage(); // Store the file in memory as a buffer
@@ -13,33 +13,43 @@ const eventController = {
       const creatorId = req.body.creatorId; // Assuming you have the user UID in the req.user object
 
       if (!eventName || !eventDateTime || !eventLocation || !coverPhoto) {
-        return res.status(400).json({ error: 'Incomplete data' });
+        return res.status(400).json({ error: "Incomplete data" });
       }
 
-      const eventId = await Event.createEvent(eventName, eventDateTime, eventLocation, coverPhoto, creatorId);
+      const eventId = await Event.createEvent(
+        eventName,
+        eventDateTime,
+        eventLocation,
+        coverPhoto,
+        creatorId
+      );
 
-      return res.status(201).json({ message: 'Event created successfully', eventId });
+      return res
+        .status(201)
+        .json({ message: "Event created successfully", eventId });
     } catch (error) {
-      console.error('Error:', error);
-      return res.status(500).json({ error: 'An error occurred' });
+      console.error("Error:", error);
+      return res.status(500).json({ error: "An error occurred" });
     }
   },
 
   uploadGalleryImage: async (req, res) => {
     try {
-      const { eventId} = req.params;
+      const { eventId } = req.params;
       const galleryImage = req.file;
 
       if (!eventId || !galleryImage) {
-        return res.status(400).json({ error: 'Incomplete data' });
+        return res.status(400).json({ error: "Incomplete data" });
       }
 
       const imageUrl = await Event.uploadGalleryImage(eventId, galleryImage);
 
-      return res.status(201).json({ message: 'Gallery image uploaded successfully', imageUrl });
+      return res
+        .status(201)
+        .json({ message: "Gallery image uploaded successfully", imageUrl });
     } catch (error) {
-      console.error('Error:', error);
-      return res.status(500).json({ error: 'An error occurred' });
+      console.error("Error:", error);
+      return res.status(500).json({ error: "An error occurred" });
     }
   },
 
@@ -49,15 +59,17 @@ const eventController = {
       const selfieImage = req.file;
 
       if (!userId || !selfieImage) {
-        return res.status(400).json({ error: 'Incomplete data' });
+        return res.status(400).json({ error: "Incomplete data" });
       }
 
       const imageUrl = await Event.uploadSelfie(userId, selfieImage);
 
-      return res.status(201).json({ message: 'Selfie image uploaded successfully', imageUrl });
+      return res
+        .status(201)
+        .json({ message: "Selfie image uploaded successfully", imageUrl });
     } catch (error) {
-      console.error('Error:', error);
-      return res.status(500).json({ error: 'An error occurred' });
+      console.error("Error:", error);
+      return res.status(500).json({ error: "An error occurred" });
     }
   },
 
@@ -66,15 +78,15 @@ const eventController = {
       const { eventId } = req.params;
 
       if (!eventId) {
-        return res.status(400).json({ error: 'Incomplete data' });
+        return res.status(400).json({ error: "Incomplete data" });
       }
 
       const galleryImages = await Event.getGalleryImages(eventId);
 
       return res.status(200).send(galleryImages);
     } catch (error) {
-      console.error('Error:', error);
-      return res.status(500).json({ error: 'An error occurred' });
+      console.error("Error:", error);
+      return res.status(500).json({ error: "An error occurred" });
     }
   },
 
@@ -82,28 +94,28 @@ const eventController = {
     try {
       const userId = req.params.userId; // Get the user ID from the URL parameters
       const events = await Event.getAll(userId);
-  
+
       return res.status(200).json(events);
     } catch (error) {
-      console.error('Error:', error);
-      return res.status(500).json({ error: 'An error occurred' });
+      console.error("Error:", error);
+      return res.status(500).json({ error: "An error occurred" });
     }
   },
 
   getSelfieImageURL: async (req, res) => {
     try {
-      const {userId} = req.params;
+      const { userId } = req.params;
 
       if (!userId) {
-        return res.status(400).json({ error: 'Incomplete data' });
+        return res.status(400).json({ error: "Incomplete data" });
       }
 
       const imageUrl = await Event.getSelfieImageURL(userId);
 
       return res.status(200).json(imageUrl);
     } catch (error) {
-      console.error('Error:', error);
-      return res.status(500).json({ error: 'An error occurred' });
+      console.error("Error:", error);
+      return res.status(500).json({ error: "An error occurred" });
     }
   },
   grantAccessToEvent: async (req, res) => {
@@ -111,37 +123,59 @@ const eventController = {
       const { userId, galleryUrl } = req.body;
       const { eventId } = req.params; // Assuming you send userId and eventId in the request body
       if (!userId || !eventId || !galleryUrl) {
-        return res.status(400).json({ error: 'Missing userId or eventId' });
+        return res.status(400).json({ error: "Missing userId or eventId" });
       }
-  
+
       await Event.grantAccessToEvent(userId, eventId, galleryUrl);
-  
-      res.status(200).json({ message: `Access granted to user ${userId} for event ${eventId}` });
+
+      res
+        .status(200)
+        .json({
+          message: `Access granted to user ${userId} for event ${eventId}`,
+        });
     } catch (error) {
-      console.error('Error granting access:', error);
-      res.status(500).json({ error: 'Internal server error' });
+      console.error("Error granting access:", error);
+      res.status(500).json({ error: "Internal server error" });
     }
   },
   getEventDetails: async (req, res) => {
     try {
-      const { userId, eventId } = req.params; 
-  
+      const { userId, eventId } = req.params;
+
       if (!userId || !eventId) {
-        return res.status(400).json({ error: 'Missing userId and eventId' });
+        return res.status(400).json({ error: "Missing userId and eventId" });
       }
-  
+
       const eventDetails = await Event.getEventDetails(userId, eventId);
-  
+
       if (eventDetails) {
-        res.status(200).json(eventDetails);  // return the whole event details
+        res.status(200).json(eventDetails); // return the whole event details
       } else {
-        res.status(404).json({ error: 'Event details not found' });
+        res.status(404).json({ error: "Event details not found" });
       }
     } catch (error) {
-      console.error('Error fetching event details:', error);
-      res.status(500).json({ error: 'Internal server error' });
+      console.error("Error fetching event details:", error);
+      res.status(500).json({ error: "Internal server error" });
     }
   },
-  
+
+  deleteGalleryImage: async (req, res) => {
+    try {
+      const { eventId, imageId } = req.params;
+
+      if (!eventId || !imageId) {
+        return res.status(400).json({ error: "Incomplete data" });
+      }
+
+      await Event.deleteGalleryImage(eventId, imageId);
+
+      return res
+        .status(200)
+        .json({ message: "Gallery image deleted successfully" });
+    } catch (error) {
+      console.error("Error:", error);
+      return res.status(500).json({ error: "An error occurred" });
+    }
+  },
 };
 module.exports = eventController;
